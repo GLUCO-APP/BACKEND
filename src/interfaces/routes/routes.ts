c91cee81524c2f9ad8796e64fbaf8f34930b3639
judar99 <judaar.99@gmail.com>
2023-04-18 11:31:39 -0500

    update get & update user with token

--- src/interfaces/routes/routes.ts
+++ src/interfaces/routes/routes.ts
@@ -25,4 +25,5 @@ router.post('/emergency/:token/:ind', emergencytcontroller.getFood.bind(emergenc
 router.post('/report',reportController.addReport.bind(reportController))
 
 
+
 export default router;
