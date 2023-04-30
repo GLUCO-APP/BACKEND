07b9c937c32176372ee9cb5406757aae8fd486bb
JuanRincon <jrincon@Falabella.com>
2023-04-30 11:20:34 -0500

    prueba socket

--- src/interfaces/controllers/testController.js
+++ src/interfaces/controllers/testController.js
@@ -10,19 +10,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.socketController = void 0;
-const socket_io_client_1 = require("socket.io-client");
-const socket = (0, socket_io_client_1.io)("http://localhost:8080");
 class socketController {
     constructor() { }
     SocketTest(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             // Escuchar un evento desde el servidor
-            const message = req.params.message;
-            socket.on("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imp1YW5AZXhhbXBsZS5jb20iLCJpYXQiOjE2ODE2MTkxMjl9.2sN6SSX6sC9OuANUhK3hbWYkwRj8BtUjV9s_kHttpzI", (msg) => {
-                console.log("Mensaje recibido desde el servidor: ", msg);
-            });
-            // Enviar un mensaje al servidor
-            socket.emit("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imp1YW5AZXhhbXBsZS5jb20iLCJpYXQiOjE2ODE2MTkxMjl9.2sN6SSX6sC9OuANUhK3hbWYkwRj8BtUjV9s_kHttpzI", message);
+            const io = req.app.get('socketIo');
+            // emit event to socket
+            io.emit('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imp1YW5AZXhhbXBsZS5jb20iLCJpYXQiOjE2ODE2MTkxMjl9.2sN6SSX6sC9OuANUhK3hbWYkwRj8BtUjV9s_kHttpzI', 'hola ');
+            res.send('Event emitted successfully');
         });
     }
 }
