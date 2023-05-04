86b0d730e76431a1e72536d8b007d98acfce116e
JuanRincon <jrincon@Falabella.com>
2023-05-03 22:42:17 -0500

    mensajitos

--- src/interfaces/controllers/testController.ts
+++ src/interfaces/controllers/testController.ts
@@ -11,11 +11,12 @@ export class socketController{
 
     public async SocketTest(req: Request, res: Response): Promise<void> {
         // Escuchar un evento desde el servidor
+        const message = req.params.message;
 
         const io: Server = req.app.get('socketIo');
   
         // emit event to socket
-        io.emit('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imp1YW5AZXhhbXBsZS5jb20iLCJpYXQiOjE2ODE2MTkxMjl9.2sN6SSX6sC9OuANUhK3hbWYkwRj8BtUjV9s_kHttpzI', 'hola ');
+        io.emit('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imp1YW5AZXhhbXBsZS5jb20iLCJpYXQiOjE2ODE2MTkxMjl9.2sN6SSX6sC9OuANUhK3hbWYkwRj8BtUjV9s_kHttpzI', message);
       
         res.send('Event emitted successfully');
     }
